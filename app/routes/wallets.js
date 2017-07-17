ce56fee2e7da7a445524948c8e121ab256ce9ebc
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-07-17 18:47:33 +0200

    add read routes for wallet

--- app/routes/wallets.js
+++ app/routes/wallets.js
@@ -4,13 +4,31 @@ const Wallet = require('../models/wallet');
 
 router.route('/')
     .post((req, res) => {
-    let wallet = new Wallet();
-    wallet.name = req.body.name;
-    wallet.currentAmount = req.body.currentAmount;
+        let wallet = new Wallet();
+        wallet.name = req.body.name;
+        wallet.currentAmount = req.body.currentAmount;
 
-    wallet.save((err) => {
-        res.send(err);
+        wallet.save((err) => {
+            res.send(err);
+        })
     })
-})
+    .get((req, res) => {
+        Wallet.find((err, wallets) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(wallets)
+        })
+    })
+
+router.route('/:id')
+    .get((req, res) => {
+        Wallet.findById(req.params.id, (err, wallet) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(wallet);
+        })
+    })
 
 module.exports = router;
