8e800c36e398d463fd078e638f5ce9972448b0dd
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-08-04 10:51:33 +0200

    add error to get wallets route

--- app/routes/wallets.js
+++ app/routes/wallets.js
@@ -18,7 +18,7 @@ router.route('/')
     .get((req, res) => {
         Wallet.find((err, wallets) => {
             if (err) {
-                res.send(err);
+                res.status(404).send({error: 'Cannot find wallets'});
             }
             res.json(wallets)
         })
